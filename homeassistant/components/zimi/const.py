4150af9a02f8a1c36d456855472c934915d5aef9
Mark Hannon <mark.hannon@gmail.com>
2024-12-27 07:02:43 +0000

    Initial import of switch platform

--- homeassistant/components/zimi/const.py
+++ homeassistant/components/zimi/const.py
@@ -4,5 +4,5 @@ from homeassistant.const import Platform
 
 CONTROLLER = "zimi_controller"
 DOMAIN = "zimi"
-PLATFORMS = [Platform.LIGHT]
+PLATFORMS = [Platform.LIGHT, Platform.SWITCH]
 TITLE = "ZIMI Controller"
